0269bfce5df8a2bac20aa11752c4dad1adb9dc83
Wafelack <wafelack@protonmail.com>
2020-10-24 11:47:24 +0200

    feat: building scanner

--- src/scanner/scanner.rs
+++ src/scanner/scanner.rs
@@ -1,25 +1,112 @@
-use crate::scanner::tokens::Tokens;
+use crate::scanner::tokens::*;
 
 struct Scanner {
-    source: Vec<String>,
-    current: u32,
-    line: u32,
-    tokens: Vec<Tokens>,
+    source: String,
+    chars: Vec<char>,
+    start: usize,
+    current: usize,
+    line: usize,
+    tokens: Vec<Token>,
 }
 impl Scanner {
-    pub fn new(source: Vec<String>) -> Scanner {
+    pub fn new(source: String) -> Scanner {
         Scanner {
-            source,
+            source: source.clone(),
+            chars: source.clone().chars().collect(),
+            start: 0,
             current: 0,
-            line: 0,
+            line: 1,
             tokens: vec![],
         }
     }
-    pub fn parse_line(&self) -> Tokens {
-        if self.source.len() == self.line as usize {
-            return Tokens::END;
-        }
+    fn is_at_end(&self) -> bool {
+        self.current >= self.source.len()
+    }
 
-        Tokens::END
+    fn add_token(&mut self, toktype: TokenType, literal: Option<String>) {
+        let text: String = self.source[self.start..self.current].into();
+        self.tokens
+            .push(Token::new(toktype, text, self.line, literal));
+    }
+    fn advance(&mut self) -> char {
+        self.current += 1;
+        self.chars[self.current - 1]
+    }
+    fn next_is(&mut self, expected: char) -> bool {
+        if self.is_at_end() {
+            return false;
+        }
+        if self.chars[self.current] != expected {
+            return false;
+        }
+        true
+    }
+    fn prev_is(&mut self, expected: char) -> bool {
+        if self.current < 2 {
+            return false;
+        }
+        if self.chars[self.current - 2] != expected {
+            return false;
+        }
+        true
+    }
+    fn peek(&self) -> char {
+        if self.is_at_end() {
+            return '\0';
+        }
+        self.chars[self.current]
+    }
+    fn peek_next(&self) -> char {
+        if self.current >= self.source.len() {
+            return '\0';
+        }
+        self.chars[self.current as usize]
+    }
+    pub fn scan_token(&mut self) {
+        let c = self.advance();
+
+        match c {
+            '(' => {
+                if self.prev_is(expected: char) {
+                    // do link stuff
+                }
+            }
+            ')' => self.add_token(TokenType::RightParen, None),
+            '[' => self.add_token(TokenType::RightBracket, None),
+            ']' => self.add_token(TokenType::LeftBracket, None),
+            '#' => {
+                if self.current != 1 {
+                    self.add_token(TokenType::Char, Some("#".to_string()))
+                } else {
+                    if self.next_is('#') {
+                        self.advance();
+                        if self.peek_next() == '#' {
+                            self.advance();
+                            let mut title = String::new();
+
+                            while !self.is_at_end() {
+                                title.push(self.advance());
+                            }
+                            self.add_token(TokenType::H3, Some(title));
+                        } else {
+                            let mut title = String::new();
+
+                            while !self.is_at_end() {
+                                title.push(self.advance());
+                            }
+                            self.add_token(TokenType::H2, Some(title));
+                        }
+                    } else {
+                        let mut title = String::new();
+
+                        while !self.is_at_end() {
+                            title.push(self.advance());
+                        }
+                        self.add_token(TokenType::H1, Some(title));
+                    }
+                }
+            }
+            '\n' => self.line += 1,
+        }
     }
 }
--- src/scanner/tokens.rs
+++ src/scanner/tokens.rs
@@ -1,9 +1,29 @@
-pub enum Tokens {
-    PARAGRAPHER(String),
-    BR,
-    VLINE,
-    IMG(String, String),
-    LINK(String, String),
-    CHAR(char),
-    END,
+pub enum TokenType {
+    Char,
+    Bang,
+    Alt,
+    Link,
+    LeftParen,
+    RightParen,
+    LeftBracket,
+    RightBracket,
+    H1,
+    H2,
+    H3,
+}
+pub struct Token {
+    toktype: TokenType,
+    lexeme: String,
+    line: usize,
+    literal: Option<String>,
+}
+impl Token {
+    pub fn new(toktype: TokenType, lexeme: String, line: usize, literal: Option<String>) -> Token {
+        Token {
+            toktype,
+            lexeme,
+            line,
+            literal,
+        }
+    }
 }
